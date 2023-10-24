4dda8456515328aa95a79105d026d81596c4d4f5
John Conrad <johnconrad314@gmail.com>
2023-10-24 12:56:14 -0500

    added descriptions/names for the swagger code gen to pick up

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 from fastapi import FastAPI, File, UploadFile, HTTPException
 
-app = FastAPI()
+app = FastAPI(
+    title="Database Normalizer API",
+    description="Class project for CS5300, spins up a FastAPI application inside a Docker container with endpoints for taking in a database schema, determining its normal form, and generating SQL queries to achieve a specified higher level of normalization.",
+)
 
 @app.post("/upload-csv/")
 async def upload_csv(file: UploadFile):
+    """Upload a CSV file containing the schema and sample data you would like to assess normalization of."""
     # Input Validation
     allowed_extensions = {'.csv'}
     _, file_extension = os.path.splitext(file.filename)
@@ -18,5 +22,6 @@ async def upload_csv(file: UploadFile):
     file_content = await file.read()
 
     # Add contents to an in Memory Database
+    # If the Database already contains a schema, raise an exception and prompt the user to a new 
 
     return {"filename": file.filename, "content_type": file.content_type}
