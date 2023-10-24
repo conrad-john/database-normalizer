52fc843537c6dc04cd815207c6215735cd3f8d8b
John Conrad <johnconrad314@gmail.com>
2023-10-24 17:34:19 -0500

    started implementing parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 from fastapi import FastAPI, File, UploadFile, HTTPException
 
 app = FastAPI(
@@ -8,8 +9,12 @@ app = FastAPI(
 
 @app.post("/upload-csv/")
 async def upload_csv(file: UploadFile):
-    """Upload a CSV file containing the schema and sample data you would like to assess normalization of."""
+    """Upload a CSV file containing the schema and sample data you would like to assess normalization of. It will be parsed and loaded into an in-memory Db."""
+
     # Input Validation
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="No File Provided.")
+
     allowed_extensions = {'.csv'}
     _, file_extension = os.path.splitext(file.filename)
     if file_extension not in allowed_extensions:
@@ -18,10 +23,38 @@ async def upload_csv(file: UploadFile):
     allowed_content_types = {'text/csv', 'application/vnd.ms-excel'}
     if file.content_type not in allowed_content_types:
         raise HTTPException(status_code=400, detail=f"File content_type is not text/csv. Content_Type: {file.content_type}")
+
+    # Read the first line of the file which should contain the attribute names
+    attribute_names = file.file.readline().decode().strip()
     
-    file_content = await file.read()
+    # Create a list of SQL queries to output
+    queries = []
 
-    # Add contents to an in Memory Database
-    # If the Database already contains a schema, raise an exception and prompt the user to a new 
+    # Initialize in-memory SQLite Database
+    connection = sqlite3.connect(':memory:')
+    cursor = connection.cursor()
 
-    return {"filename": file.filename, "content_type": file.content_type}
+    # Create a temporary relation with the given attribute names
+    table_name = "TemporaryRelation"
+    create_table_query = f"CREATE TABLE {table_name} ({attribute_names})"
+    cursor.execute(create_table_query)
+    queries.append(create_table_query)
+
+    # Insert each record provided into the table we just created
+    while True:
+        line = file.file.readline().decode().strip()
+        if not line:
+            # End of File
+            break
+        insert_query = f"INSERT INTO {table_name} ({attribute_names}) VALUES ({line})"
+        cursor.execute(insert_query)
+        queries.append(insert_query)
+
+    # Commit the Changes to the Database
+    connection.commit()
+
+    # Close the Database Connection - this should remove the in-memory database
+    connection.close()
+
+    return {"queries_ran": queries}
+    #return {"filename": file.filename, "content_type": file.content_type}
