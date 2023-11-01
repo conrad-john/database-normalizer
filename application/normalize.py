5681da3fff02ae1638934d154553b20aa3e283a0
John Conrad <johnconrad314@gmail.com>
2023-10-31 21:06:23 -0500

    implemented 1NF normalizer

--- application/normalize.py
+++ application/normalize.py
@@ -6,6 +6,7 @@ from core.attribute import Attribute
 from application.determine_normal_form import determine_normal_form, isRelationIn1NF, isRelationIn2NF, isRelationIn3NF, isRelationInBCNF, isRelationIn4NF, isRelationIn5NF
 
 async def normalize(relation: Relation, dependencies: List[Dependency], target_nf: str, current_nf: str) -> List[Relation]:
+    # Convert/Get NF Integers for easier comparison
     target = get_nf_integer(target_nf)
     current = get_nf_integer(current_nf) if current_nf != "N/A" else get_nf_integer(determine_normal_form(relation))
     
@@ -89,23 +90,65 @@ async def normalize(relation: Relation, dependencies: List[Dependency], target_n
 
     return subrelations
 
-async def normalize_to_1NF(relation: Relation, dependencies: List[Dependency]) -> List[Relation]:
-    
+async def normalize_to_1NF(relation: Relation) -> List[Relation]:
+    # Are all values atomic and are there any duplicate Attribute Names?
+    attribute_names = []
+    for attribute in relation.attributes:
+        # If there's a non-atomic value, assume an error with the parser and set the bool to True so we don't get repeat warning logs.
+        if not attribute.isAtomic:
+            print(f"Warning: non-atomic value detected. Attribute: '{attribute.name}'. The data contract provided is not strict enough to properly enforce atomic values so I'm assuming error in my ability to parse your desired input.")
+            attribute.isAtomic = True
+        # If there's a duplicate attribute name
+        if attribute.name in attribute_names:
+            print(f"Relation is in UNF: {attribute.name} appears more than once as a column name.")
+            # Alter the name and duplicate the dependencies
+            renamed_attribute = f"Duplicate_{attribute.name}"
+            new_dependencies = []
+            for dependency in relation.dependencies:
+                if attribute.name in dependency.parent:
+                    new_dependencies.append(Dependency(parent=renamed_attribute, children=dependency.children))
+                elif attribute.name in dependency.children:
+                    stepchildren = [children for children in dependency.children if children != attribute.name]
+                    stepchildren.append(renamed_attribute)
+                    new_dependencies.append(Dependency(parent=dependency.parent, children=stepchildren))
+            attribute.name = renamed_attribute
+            relation.dependencies.append(new_dependencies)
+            # Just in case the name is duplicated more than once, we'll end up with 'Duplicate_...Duplicate_{attribute.name}
+            attribute_names.append(renamed_attribute)
+        else:
+            attribute_names.append(attribute.name)
+
+    # Are all tuples unique? If not, remove duplicate data
+    data_rows = []
+    duplicate_row_indexes = []
+    for index, t in enumerate(relation.tuples):
+        row = ', '.join(t)
+        if row in data_rows:
+            duplicate_row_indexes.append(index)
+        data_rows.append(row)
+    if len(duplicate_row_indexes) > 0:
+        deduped_rows = []
+        for index, t in enumerate(relation.tuples):
+            if index in duplicate_row_indexes:
+                continue
+            deduped_rows.append(t)
+        relation.tuples = deduped_rows
+
+    return [relation]
+
+async def normalize_to_2NF(relation: Relation) -> List[Relation]:
     raise NotImplementedError()
 
-async def normalize_to_2NF(relation: Relation, dependencies: List[Dependency]) -> List[Relation]:
+async def normalize_to_3NF(relation: Relation) -> List[Relation]:
     raise NotImplementedError()
 
-async def normalize_to_3NF(relation: Relation, dependencies: List[Dependency]) -> List[Relation]:
+async def normalize_to_BCNF(relation: Relation) -> List[Relation]:
     raise NotImplementedError()
 
-async def normalize_to_BCNF(relation: Relation, dependencies: List[Dependency]) -> List[Relation]:
+async def normalize_to_4NF(relation: Relation) -> List[Relation]:
     raise NotImplementedError()
 
-async def normalize_to_4NF(relation: Relation, dependencies: List[Dependency]) -> List[Relation]:
-    raise NotImplementedError()
-
-async def normalize_to_5NF(relation: Relation, dependencies: List[Dependency]) -> List[Relation]:
+async def normalize_to_5NF(relation: Relation) -> List[Relation]:
     raise NotImplementedError()
 
 def get_nf_integer(nf: str) -> int:
