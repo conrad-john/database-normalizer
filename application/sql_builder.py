b76bafbb6cdb62738c838034b3494e9434011d0b
John Conrad <johnconrad314@gmail.com>
2023-11-03 02:49:02 -0500

    fixed SQL create table builder

--- application/sql_builder.py
+++ application/sql_builder.py
@@ -2,7 +2,17 @@ from typing import List
 from core.relation import Relation
 
 def get_table_creation_queries(relations: List[Relation]) -> List[str]:
-    queries = []
+    sql_queries = []
+
     for relation in relations:
-        queries.append(relation.generate_create_table_query())
-    return queries
+        attributes_sql = ", ".join(f"{attribute.name} {attribute.data_type}" for attribute in relation.attributes)
+        primary_keys_sql = ", ".join(attribute.name for attribute in relation.primary_keys) if relation.primary_keys else ""
+
+        create_table_sql = f"CREATE TABLE {relation.name} ({attributes_sql}"
+        if primary_keys_sql:
+            create_table_sql += f", PRIMARY KEY ({primary_keys_sql})"
+        create_table_sql += ");"
+        
+        sql_queries.append(create_table_sql)
+
+    return sql_queries
